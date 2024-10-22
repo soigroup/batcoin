7b613891fafa6bdb40a3455c4cdbe1b3bfa8c203
soigroup <168447232+soigroup@users.noreply.github.com>
2024-10-22 12:39:02 +0500

    Delete bot directory

--- bot/constants.ts
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-export const PORT = Number(process.env.PORT) || 3000;
-export const APP_URL = String(process.env.APP_URL);
-export const TELEGRAM_TOKEN = String(process.env.TELEGRAM_TOKEN);
