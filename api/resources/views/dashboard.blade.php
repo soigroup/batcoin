97fe64e015d535bfbe07ed33938aa6cd4ce6c22d
soigroup <168447232+soigroup@users.noreply.github.com>
2024-10-24 16:22:25 +0500

    Delete api directory

--- api/resources/views/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-
-@section('content')
-    <h1 class="text-2xl font-bold mb-4">Admin Dashboard</h1>
-    <p>Total Users: {{ $userCount }}</p>
-    <p>Total Tasks: {{ $taskCount }}</p>
-    <p>Total Daily Tasks: {{ $dailyTaskCount }}</p>
-@endsection
