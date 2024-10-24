b4a104fa2ac61d0b0ed95df3b98c68b2271d1461
soigroup <168447232+soigroup@users.noreply.github.com>
2024-10-24 14:58:12 +0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -116,8 +116,10 @@ function App() {
     signIn().then(() => setShowSplashScreen(false));
   }, [user]);
 
+  if (!user || isDisktop) return <PlayOnYourMobile />;
 
   if (showSplashScreen) return <SplashScreen />;
+
   if (isFirstLoad)
     return <FirstTimeScreen startGame={() => setIsFirstLoad(false)} />;
 
