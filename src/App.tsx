5791ca52471344138a5f17fccccc44de13eb675b
soigroup <168447232+soigroup@users.noreply.github.com>
2024-10-24 14:32:00 +0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,11 @@ import { $http, setBearerToken } from "./lib/http";
 import { BoosterType, BoosterTypes, UserType } from "./types/UserType";
 import { useUserStore } from "./store/user-store";
 import { uesStore } from "./store";
-import PlayOnYourMobile from "./pages/PlayOnYourMobile";
 import { useDebounce } from "@uidotdev/usehooks";
 import { toast } from "react-toastify";
 import useTelegramInitData from "./hooks/useTelegramInitData";
 
 const webApp = window.Telegram.WebApp;
-const isDisktop = import.meta.env.DEV
-  ? false
-  : Telegram.WebApp.platform === "tdesktop";
 
 function App() {
   const userStore = useUserStore();
@@ -116,12 +112,12 @@ function App() {
     signIn().then(() => setShowSplashScreen(false));
   }, [user]);
 
-
   if (showSplashScreen) return <SplashScreen />;
 
   if (isFirstLoad)
     return <FirstTimeScreen startGame={() => setIsFirstLoad(false)} />;
 
+  // بدء اللعبة مباشرةً إذا كان المستخدم موجودًا
   return <RouterProvider router={router} />;
 }
 
