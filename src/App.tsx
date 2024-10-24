9e9c9e6ae72738bed5e503634adc40d31a4f1a5e
soigroup <168447232+soigroup@users.noreply.github.com>
2024-10-24 14:28:06 +0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,6 @@ function App() {
     signIn().then(() => setShowSplashScreen(false));
   }, [user]);
 
-  if (!user || isDisktop) return <PlayOnYourMobile />;
 
   if (showSplashScreen) return <SplashScreen />;
 
